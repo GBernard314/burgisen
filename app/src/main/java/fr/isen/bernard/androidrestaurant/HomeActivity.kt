33680d63159ced765262da47735def1a23dafb36
gbernard314 <guillaume.bernard@isen.yncrea.fr>
2021-01-25 13:08:22 +0100

    adding pages and redirection

--- app/src/main/java/fr/isen/bernard/androidrestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/bernard/androidrestaurant/HomeActivity.kt
@@ -1,20 +1,38 @@
 package fr.isen.bernard.androidrestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
 
 class HomeActivity : AppCompatActivity() {
+
+    fun displayMsg(str: String){
+        Toast.makeText(this, "Selected : $str", Toast.LENGTH_SHORT).show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun toastMsg(msg: String?) {
-        val toast: Toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
-        toast.show()
-    }
+    fun onClick(v: View){
 
-    fun displayToastMsg(v: View?) {
-        toastMsg("Hello how are you today!!")
+        when (v.id) {
+            R.id.buttonEntrees -> {
+                displayMsg("Entrees")
+                startActivity(Intent(this, EntreesActivity::class.java))
+            }
+            R.id.buttonPlats -> {
+                displayMsg("Plats")
+                startActivity(Intent(this, PlatsActivity::class.java))
+            }
+            R.id.buttonDesserts -> {
+                displayMsg("Desserts")
+                startActivity(Intent(this, DessertsActivity::class.java))
+            }
+        }
     }
 }
