4b4f5c16a3f1e10435eb76761b488c9be3ade476
gbernard314 <guillaume.bernard@isen.yncrea.fr>
2021-01-25 11:11:19 +0100

    needed

---
+++ app/build/generated/source/buildConfig/debug/fr/isen/bernard/androidrestaurant/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package fr.isen.bernard.androidrestaurant;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "fr.isen.bernard.androidrestaurant";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
